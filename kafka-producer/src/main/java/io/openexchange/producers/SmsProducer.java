a3228465595996cb6a9045f5bc1eb90615d56614
andriyka <andriyka@playtech.com>
2017-02-07 11:56:22 +0000

    [ISSUE #19]

--- kafka-producer/src/main/java/io/openexchange/producers/SmsProducer.java
+++ kafka-producer/src/main/java/io/openexchange/producers/SmsProducer.java
@@ -2,6 +2,10 @@ package io.openexchange.producers;
 
 import io.openexchange.configuration.SmsChannel;
 import io.openexchange.pojos.Sms;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +15,18 @@ import java.util.UUID;
 
 @Component
 public class SmsProducer {
-    @SendTo(SmsChannel.SMS)
-    public Sms produce(String to, String from, String text){
+    private MessageChannel output;
+
+    @Autowired
+    public SmsProducer(@Qualifier(SmsChannel.SMS) MessageChannel output) {
+        this.output = output;
+    }
+
+    public void produce(String to, String from, String text) {
+        output.send(MessageBuilder.withPayload(of(to, from, text)).build());
+    }
+
+    private static Sms of(String to, String from, String text){
         return new Sms()
                 .withMessageId(UUID.randomUUID())
                 .withMobileTerminate(to)
--- kafka-producer/src/test/java/io/openexchange/SmsStreamApplicationTest.java
+++ kafka-producer/src/test/java/io/openexchange/SmsStreamApplicationTest.java
@@ -1,40 +1,64 @@
 package io.openexchange;
 
 import io.openexchange.configuration.SmsChannel;
+import io.openexchange.pojos.Sms;
+import io.openexchange.producers.SmsProducer;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(KafkaProducerApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 @TestPropertySource(locations = "classpath:test.properties")
 public class SmsStreamApplicationTest {
     @Autowired
     private SmsChannel channel;
+    @Autowired
+    private SmsProducer smsProducer;
 
     @ClassRule
-    // Alternative: docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=`docker-machine ip \`docker-machine active\`` --env ADVERTISED_PORT=9092 spotify/kafka
     public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true);
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        System.setProperty("spring.cloud.stream.kafka.binder.defaultZkPort",
-                Integer.toString(embeddedKafka.getZookeeper().port()));
+        System.setProperty("spring.cloud.stream.kafka.binder.defaultZkPort", Integer.toString(embeddedKafka.getZookeeper().port()));
+        System.setProperty("spring.cloud.stream.kafka.binder.brokers", embeddedKafka.getBrokersAsString());
+        System.out.println();
     }
 
     @Test
     public void contextLoads() {
         assertNotNull(this.channel.sms());
+    }
+
+    @Test
+    public void testSendReceive() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        channel.sms().subscribe(message -> {
+            assertTrue(message.getPayload() instanceof Sms);
+            Sms sms = (Sms) message.getPayload();
+            assertEquals("+3725811223344", sms.getMobileTerminate());
+            assertEquals("+3725844332211", sms.getMobileOriginate());
+            assertEquals("Test sms", sms.getText());
+            assertNotNull(sms.getMessageId());
+            assertNotNull(sms.getReceiveTime());
+            latch.countDown();
+        });
+        smsProducer.produce("+3725811223344", "+3725844332211", "Test sms");
+        assertTrue(latch.await(5000, TimeUnit.MILLISECONDS));
     }
 }
